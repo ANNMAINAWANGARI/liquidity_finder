035f2abc541e2e748bde84f0106977e32c70dfcc
ANNMAINAWANGARI <wangariann371@gmail.com>
2024-01-12 13:21:25 +0300

    created a global context and fetched the pool address+ pool data using the polygon rpc the added it to the context

--- liquidity_finder/context/index.js
+++ liquidity_finder/context/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import {ethers,Contract} from 'ethers';
 import axios from 'axios';
 import UniswapV3Pool from '@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json';
@@ -16,8 +16,98 @@ export const CONTEXT_PROVIDER = ({children})=>{
     const [loader,setLoader] = useState(false);
 
     const notifyError = (msg)=> toast.error(msg, {duration:4000})
-    return <CONTEXT.Provider value={{}}>
-        {childern}
+    const notifySuccess = (msg)=> toast.success(msg, {duration:4000})
+
+
+    //GET POOL ADDRESS
+    const GET_POOL_ADDRESS = async(liquidity,selectedNetwork)=>{
+        try{
+            setLoader(true);
+            const PROVIDER = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/polygon_mumbai');
+            const FACTORY_CONTRACT = new ethers.Contract(FACTORY_ADDRESS,FACTORY_ABI,PROVIDER);
+            
+            const POOL_ADDRESS = await FACTORY_CONTRACT.functions.getPool(
+                liquidity.token_A,
+                liquidity.token_B,
+                Number(liquidity.fee),
+            )
+            const POOL_HISTORY = {
+                token_A:liquidity.token_A,
+                token_B:liquidity.token_B,
+                fee:liquidity.fee,
+                network:selectedNetwork.name,
+                poolAddress:POOL_ADDRESS
+            }
+            
+            let poolArray = [];
+            const POOL_LIST = localStorage.getItem('poolHistory') ;
+            if(POOL_LIST){
+                poolArray = JSON.parse(localStorage.getItem('poolHistory'));
+                poolArray.push(POOL_HISTORY);
+                localStorage.setItem('poolHistory',JSON.stringify(poolArray))
+            }else{
+                poolArray.push(POOL_HISTORY);
+                localStorage.setItem('poolHistory',JSON.stringify(poolArray))
+            }
+            setLoader(false);
+            notifySuccess('Successfully Completed')
+            return POOL_ADDRESS
+        }catch(error){
+            setLoader(false);
+            notifyError('error')
+        }
+    }
+
+    //GET POOL DATA
+    async function GET_POOL_DATA(poolContract,selectedNetwork,poolAddress){
+        const [liquidity,fee,token0,token1] = await Promise.all([
+            poolContract.liquidity(),
+            poolContract.fee(),
+            poolContract.token0(),
+            poolContract.token1()
+        ])
+        return {
+            liquidity: liquidity.toString(),
+            fee: fee,
+            token_A: token0,
+            token_B: token1,
+            network: selectedNetwork.name,
+            poolAddress: poolAddress
+        }
+    }
+
+    const GET_POOL_DETAILS = async(poolAddress, selectedNetwork)=>{
+        try{
+            setLoader(true);
+            const PROVIDER = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/polygon_mumbai');
+            const poolContract = new Contract(poolAddress,UniswapV3Pool.abi,PROVIDER);
+
+            const  poolData = await GET_POOL_DATA(poolContract,selectedNetwork,poolAddress)
+            let liquidityArray = [];
+            const POOL_LIST = localStorage.getItem('liquidityHistory') ;
+            if(POOL_LIST){
+                liquidityArray = JSON.parse(localStorage.getItem('liquidityHistory'));
+                liquidityArray.push(poolData);
+                localStorage.setItem('liquidityHistory',JSON.stringify(poolArray))
+            }else{
+                liquidityArray.push(poolData);
+                localStorage.setItem('liquidityHistory',JSON.stringify(poolArray))
+            }
+
+            setLoader(false);
+            notifySuccess('Successfully Completed')
+            return poolData
+        }catch(err){
+            setLoader(false);
+            notifySuccess('Error')
+        }
+    }
+
+    
+
+    
+    return <CONTEXT.Provider value={{DAPP_NAME, GET_POOL_ADDRESS, GET_POOL_DETAILS, loader}}>
+        {children}
     </CONTEXT.Provider>
 }
 
--- liquidity_finder/pages/index.js
+++ liquidity_finder/pages/index.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React,{useState,useContext,useEffect} from 'react';
+import {
+  Header,
+  Home,
+  Action,
+  GetPool,
+  Networks,
+  LiqudityHistory,
+  PoolHistory,
+  Promo,
+  Loader,
+  IconOne,
+  IconTwo,
+} from '../components/index';
+import {CONTEXT}from '../context/index'
 
 const index = () => {
+
+  const {DAPP_NAME} = useContext(CONTEXT);
   return (
-    <div>index</div>
+    <div>{DAPP_NAME}</div>
   )
 }
 
