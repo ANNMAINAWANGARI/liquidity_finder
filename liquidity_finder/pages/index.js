1e8a50a5bb53800e4b79c2838d22e6fec558ee86
ANNMAINAWANGARI <wangariann371@gmail.com>
2024-01-12 15:06:21 +0300

    built dynamic components

--- liquidity_finder/pages/index.js
+++ liquidity_finder/pages/index.js
@@ -16,9 +16,38 @@ import {CONTEXT}from '../context/index'
 
 const index = () => {
 
-  const {DAPP_NAME} = useContext(CONTEXT);
+  const {DAPP_NAME,loader,GET_POOL_ADDRESS, GET_POOL_DETAILS} = useContext(CONTEXT);
+  const [activeNetwork,setActiveNetwork] = useState('');
+  const [activeComponent,setActiveComponent] = useState('Home');
+  useEffect(()=>{
+    const network = JSON.parse(localStorage.getItem("activeNetwork"));
+    setActiveNetwork(network?.name)
+  },[activeNetwork])
   return (
-    <div>{DAPP_NAME}</div>
+    <div className='bg-slate-900'>
+      <Header setActiveComponent={setActiveComponent} activeNetwork={activeNetwork}/>
+      {
+        activeComponent ==='Home'?(
+         <Home 
+         setActiveComponent={setActiveComponent}
+         GET_POOL_DETAILS = {GET_POOL_DETAILS}/>
+        ):activeComponent ==='Liquidity'?(
+          <GetPool GET_POOL_ADDRESS = {GET_POOL_ADDRESS}/>
+        ):activeComponent ==='Pool History'?(
+          <PoolHistory setActiveComponent={setActiveComponent}/>
+        ):activeComponent ==='Liquidity History'?(
+          <LiqudityHistory setActiveComponent={setActiveComponent}/>
+        ):activeComponent ==='Networks'?(
+          <Networks setActiveComponent={setActiveComponent} activeNetwork={activeNetwork}/>
+        ):(<></>)
+      }
+      <Action/>
+      {
+        loader &&(
+          <div className='new_loader'><Loader/></div>
+        )
+      }
+    </div>
   )
 }
 
